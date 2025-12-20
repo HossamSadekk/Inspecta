9675d062ab62e02f7ac2c33308f4a3cad312c174
Hossamsadekk <66652255+HossamSadekk@users.noreply.github.com>
2025-12-20 16:16:56 +0200

    handle gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,34 @@
 plugins {
+    `maven-publish`
+    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.2.1"
     kotlin("jvm") version "2.0.0"
 }
 
-group = "org.plugin"
-version = "1.0-SNAPSHOT"
+group = "com.inspecta.plugin"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    google()
 }
 
+gradlePlugin {
+    website.set("https://github.com/HossamSadekk/Inspecta")
+    vcsUrl.set("https://github.com/HossamSadekk/Inspecta")
+
+    plugins {
+        create("inspecta") {
+            id = "com.inspecta.plugin"
+            implementationClass = "com.inspecta.plugin.InspectaPlugin"
+            displayName = "Inspecta"
+            description = "A powerful CLI tool to audit Android app size, assets, libraries, and unused resources."
+            tags.set(listOf("android", "analytics", "size", "bloat", "assets"))
+        }
+    }
+}
 dependencies {
+    implementation("com.android.tools.build:gradle:8.3.0")
     testImplementation(kotlin("test"))
 }
 
